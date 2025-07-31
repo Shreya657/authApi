c8c6d0e678a8b22d92438e70bf4ab326faf5a3b9
shreya <berashreya05@gmail.com>
2025-07-31 19:39:03 +0530

    gg

--- src/app.js
+++ src/app.js
@@ -10,6 +10,21 @@ app.use(cors(
 
 ))
 
+
+
+// const allowedOrigins = process.env.CORS_ORIGIN.split(',');
+
+// app.use(cors({
+//   origin: function (origin, callback) {
+//     if (!origin || allowedOrigins.includes(origin)) {
+//       callback(null, true);
+//     } else {
+//       callback(new Error('Not allowed by CORS'));
+//     }
+//   },
+//   credentials: true
+// }));
+
 app.use(express.json({limit:"16kb"}))
 app.use(express.urlencoded({extended:true,limit:"16kb"}))
 app.use(express.static("public"))
