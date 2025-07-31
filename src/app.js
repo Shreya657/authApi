a84d379d683e623aa89ba4549eff27d43d5262ce
shreya <berashreya05@gmail.com>
2025-07-31 23:41:53 +0530

    changed in cors

--- src/app.js
+++ src/app.js
@@ -2,28 +2,27 @@ import express from "express"
 import cors from "cors"
 import cookieParser from "cookie-parser"
 const app=express()
-app.use(cors(
-    {
-        origin:process.env.CORS_ORIGIN,
-        credentials:true
-    }
-
-))
-
-
-
-// const allowedOrigins = process.env.CORS_ORIGIN.split(',');
-
-// app.use(cors({
-//   origin: function (origin, callback) {
-//     if (!origin || allowedOrigins.includes(origin)) {
-//       callback(null, true);
-//     } else {
-//       callback(new Error('Not allowed by CORS'));
+// app.use(cors(
+//     {
+//         origin:process.env.CORS_ORIGIN,
+//         credentials:true
 //     }
-//   },
-//   credentials: true
-// }));
+
+// ))
+
+
+
+const allowedOrigins = (process.env.CORS_ORIGIN || '').split(',');
+app.use(cors({
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+  credentials: true
+}));
 
 app.use(express.json({limit:"16kb"}))
 app.use(express.urlencoded({extended:true,limit:"16kb"}))
